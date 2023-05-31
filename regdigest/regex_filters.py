88d4c64bb0bf148afd5565d3d94770fbde539f4c
Mark Febrizio <63374265+mfebrizio@users.noreply.github.com>
2023-05-31 14:53:55 -0400

    Create regex_filters.py
    constants for regex searches

---
+++ regdigest/regex_filters.py
@@ -0,0 +1,17 @@
+# title filters for routine actions
+FILTER_ROUTINE = [  
+    r"^Privacy\sAct\sof\s1974", 
+    r"^Airworthiness\sDirectives", 
+    r"^Airworthiness\sCriteria", 
+    r"^Fisheries\sof\sthe\s[\w]+;", 
+    r"^Submission\sfor\sOMB\sReview;", 
+    r"^Sunshine\sAct\sMeetings", 
+    r"^Agency\sInformation\sCollection\sActivit[\w]+", 
+    r"Notice\sof\sClosed\sMeetings$", 
+    r"^Environmental\sImpact\sStatements;\sNotice\sof\sAvailability", 
+    r"^Combined\sNotice\sof\sFilings", 
+    r"Coastwise\sEndorsement\sEligibility\sDetermination\s.+\sVessel", 
+    r"^Safety\sZone[\w]*", 
+    r"^Air\sPlan\sApproval", 
+    r"^Drawbridge\sOperation\sRegulation", 
+    ]
