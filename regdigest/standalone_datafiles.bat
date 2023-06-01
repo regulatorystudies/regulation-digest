fab8bf4d1b287bdc2c2c436a948a3831cfec9d4d
Mark Febrizio <63374265+mfebrizio@users.noreply.github.com>
2023-06-01 16:33:49 -0400

    reorg bat files

--- regdigest/standalone_datafiles.bat
+++
@@ -1,5 +0,0 @@
-cd ".\Box Sync\_MF\Reg Digest\regulation-digest\regdigest\"
-
-conda activate regdigest
-
-python -m nuitka --standalone --remove-output --include-data-files={MAIN_DIRECTORY}/data/agencies_endpoint_metadata.json=data/agencies_endpoint_metadata.json retrieve_documents.py
--- regdigest/standalone_onefile_tempdir.bat
+++
@@ -1,1 +0,0 @@
-python -m nuitka --standalone --onefile --remove-output --onefile-tempdir-spec='%CACHE_DIR%/' retrieve_documents.py
