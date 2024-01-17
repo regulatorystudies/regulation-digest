2e92d348264f46d015f09c32aff91e845b37e307
Mark Febrizio <63374265+mfebrizio@users.noreply.github.com>
2024-01-17 15:16:20 -0500

    Update nuitka_standalone.bat
    use && for multiple conditional commands

--- nuitka_standalone.bat
+++ nuitka_standalone.bat
@@ -1,5 +1,1 @@
-conda activate regdigest
-
-python -m nuitka --standalone --remove-output --include-data-files=./regdigest/data/agencies_endpoint_metadata.json=./data/agencies_endpoint_metadata.json ./regdigest/retrieve_clips_program.py
-
-python create_program_zip.py
+conda activate regdigest && python -m nuitka --standalone --remove-output --include-data-files=./regdigest/data/agencies_endpoint_metadata.json=./data/agencies_endpoint_metadata.json ./regdigest/retrieve_clips_program.py && python create_program_zip.py
