d1aefcbf582d5c8132232de249bd6d83ccd72dfd
mfebrizio <mfebrizio@gwu.edu>
2024-06-12 12:42:16 -0400

    Update nuitka_standalone.bat
    remove activate command; add option to avoid bundling ipython

--- nuitka_standalone.bat
+++ nuitka_standalone.bat
@@ -1,1 +1,1 @@
-conda activate regdigest && python -m nuitka --standalone --remove-output ./regdigest/retrieve_clips_program.py && python create_program_zip.py
+python -m nuitka --standalone --remove-output --nofollow-import-to=ipython ./regdigest/retrieve_clips_program.py && python create_program_zip.py
