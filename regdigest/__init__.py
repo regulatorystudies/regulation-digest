451b0e6001131b149b4ea9c694c16e49697e0a53
mfebrizio <mfebrizio@gwu.edu>
2024-06-13 17:42:21 -0400

    cleanup

--- regdigest/__init__.py
+++ regdigest/__init__.py
@@ -1,7 +1,7 @@
 __release__ = {
     "title": "regulation-digest", 
     "description": "Program to collect Federal Register documents for the weekly Regulation Digest newsletter.", 
-    "version": "0.2.0", 
+    "version": "0.3.0", 
     "author": "Mark Febrizio", 
     "license": "MIT License"
     }
