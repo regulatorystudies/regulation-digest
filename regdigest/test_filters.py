d9ea82a173139d1dfec286e103dc8f3e5b252a2e
mfebrizio <mfebrizio@gwu.edu>
2024-06-28 13:04:32 -0400

    Update test_filters.py

--- regdigest/test_filters.py
+++ regdigest/test_filters.py
@@ -1,9 +1,17 @@
+from pathlib import Path
+from datetime import date
+
 from retrieve_documents import retrieve_documents
+
 
 if __name__ == "__main__":
     
     start = input("Start date [yyyy-mm-dd]: ")
     end = input("End date [yyyy-mm-dd]: ")
+    download = input("Download? [yes/no]: ")
 
     df = retrieve_documents(start_date=start, end_date=end, test_filters=True)
     print(df.head())
+    if download.lower() in ("yes", "y"):
+        outpath = Path(__file__).parents[1].joinpath("output")
+        df.to_csv(outpath / f"removed_documents_{date.today()}.csv", index=False)
