729ba9721fb4852c0fa9e1eebf7502eff1518c2b
mfebrizio <mfebrizio@gwu.edu>
2024-07-30 13:31:43 -0400

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,1 @@
-rsconnect deploy shiny .\regdigest\ --name regulatorystudies --title regulation-digest --force-generate
+rsconnect deploy shiny .\regdigest\ --name regulatorystudies --title regulation-digest --app-id 12182754 --force-generate
